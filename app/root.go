b07d601122e674ac04b13feb6aac8a0e9432b11c
Miquel Sabaté <mikisabate@gmail.com>
2014-09-24 11:21:09 +0200

    app: added the proper documentation to the RootIndex function.

--- app/root.go
+++ app/root.go
@@ -8,18 +8,22 @@ import (
 	"net/http"
 )
 
-// TODO
+// Renders the root page. It has three different options:
+//
+//  1. If there's no user, it renders the "Create user" page.
+//  2. If the current user is not logged in, it render the "Login" page.
+//  3. If the current user is logged in, then it redirects the user to the
+//     /topics page.
 func RootIndex(res http.ResponseWriter, req *http.Request) {
 	s, _ := store.Get(req, sessionName)
 	id := s.Values["userId"]
 
 	if id == nil {
-		o := &Options{}
 		count, err := Db.SelectInt("select count(*) from users")
 		if err == nil && count == 0 {
-			render(res, "users/new", o)
+			render(res, "users/new", &Options{})
 		} else {
-			render(res, "application/login", o)
+			render(res, "application/login", &Options{})
 		}
 	} else {
 		http.Redirect(res, req, "/topics", http.StatusFound)
