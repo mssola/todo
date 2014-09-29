b01479d9dcf7640f3713288bd34ed2c3f108156e
Miquel Sabaté <mikisabate@gmail.com>
2014-09-29 21:54:03 +0200

    app: added some tests to the topics resource.

--- app/topics_test.go
+++ app/topics_test.go
@@ -10,6 +10,7 @@ import (
 	"net/url"
 	"testing"
 
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,4 +59,103 @@ func TestTopicsCreateNoName(t *testing.T) {
 	assert.NotNil(t, err)
 	count, err := Db.SelectInt("select count(*) from users")
 	assert.Equal(t, count, 0)
+}
+
+func TestTopicsRename(t *testing.T) {
+	InitTestDB()
+	defer CloseTestDB()
+
+	err := createTopic("topic")
+	assert.Nil(t, err)
+
+	var t1, t2 Topic
+	err = Db.SelectOne(&t1, "select * from topics")
+	assert.Nil(t, err)
+
+	param := make(url.Values)
+
+	req, err := http.NewRequest("POST", "/topics/"+t1.Id, nil)
+	assert.Nil(t, err)
+	param["name"] = []string{"topic1"}
+	req.PostForm = param
+	w := httptest.NewRecorder()
+
+	m := mux.NewRouter()
+	m.HandleFunc("/topics/{id}", TopicsUpdate)
+	m.ServeHTTP(w, req)
+
+	// DB
+	err = Db.SelectOne(&t2, "select * from topics")
+	assert.Nil(t, err)
+	assert.Equal(t, t2.Name, "topic1")
+	assert.Equal(t, t1.Id, t2.Id)
+
+	// HTTP
+	assert.Equal(t, w.Code, 302)
+	assert.Equal(t, w.HeaderMap["Location"][0], "/topics")
+}
+
+func TestUpdateContents(t *testing.T) {
+	InitTestDB()
+	defer CloseTestDB()
+
+	err := createTopic("topic")
+	assert.Nil(t, err)
+
+	var t1, t2 Topic
+	err = Db.SelectOne(&t1, "select * from topics")
+	assert.Nil(t, err)
+
+	param := make(url.Values)
+
+	req, err := http.NewRequest("POST", "/topics/"+t1.Id, nil)
+	assert.Nil(t, err)
+	param["contents"] = []string{"**bold**"}
+	req.PostForm = param
+	w := httptest.NewRecorder()
+
+	m := mux.NewRouter()
+	m.HandleFunc("/topics/{id}", TopicsUpdate)
+	m.ServeHTTP(w, req)
+
+	// DB
+	err = Db.SelectOne(&t2, "select * from topics")
+	assert.Nil(t, err)
+	assert.Equal(t, t1.Name, t2.Name)
+	assert.Equal(t, t1.Id, t2.Id)
+	assert.NotEqual(t, t1.Contents, t2.Contents)
+	assert.Equal(t, t2.Contents, "**bold**")
+
+	// HTTP
+	assert.Equal(t, w.Code, 302)
+	assert.Equal(t, w.HeaderMap["Location"][0], "/topics")
+}
+
+func TestDestroy(t *testing.T) {
+	InitTestDB()
+	defer CloseTestDB()
+
+	err := createTopic("topic")
+	assert.Nil(t, err)
+
+	var t1 Topic
+	err = Db.SelectOne(&t1, "select * from topics")
+	assert.Nil(t, err)
+
+	req, err := http.NewRequest("POST", "/topics/"+t1.Id+"/delete", nil)
+	assert.Nil(t, err)
+	w := httptest.NewRecorder()
+
+	m := mux.NewRouter()
+	m.HandleFunc("/topics/{id}/delete", TopicsDestroy)
+	m.ServeHTTP(w, req)
+
+	// DB
+	c, err := Db.SelectInt("select count(*) from topics")
+	assert.Nil(t, err)
+	assert.Equal(t, c, 0)
+
+	// HTTP
+	assert.Equal(t, w.Code, 302)
+	assert.Equal(t, w.HeaderMap["Location"][0], "/topics")
 }
