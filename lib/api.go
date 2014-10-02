e0c620888d139dfb34e87bfa5ff0985c371d51ae
Miquel Sabaté <mikisabate@gmail.com>
2014-10-02 21:10:29 +0200

    lib: added documentation for the api.go file.

--- lib/api.go
+++ lib/api.go
@@ -32,7 +32,8 @@ func JsonError(res http.ResponseWriter) {
 	fmt.Fprint(res, Response{Error: "Failed!"})
 }
 
-// TODO: not sure about this one.
+// Returns true and sends the proper response if the given error is not nil. If
+// the error is nil, then it just returns false and does nothing.
 func CheckError(res http.ResponseWriter, req *http.Request, err error) bool {
 	if err == nil {
 		return false
@@ -47,6 +48,7 @@ func CheckError(res http.ResponseWriter, req *http.Request, err error) bool {
 	return true
 }
 
+// Returns true if the given header key has "application/json" as its value.
 func checkHeader(req *http.Request, name string) bool {
 	ct := req.Header[name]
 	if len(ct) != 1 {
@@ -55,7 +57,7 @@ func checkHeader(req *http.Request, name string) bool {
 	return ct[0] == "application/json"
 }
 
-// TODO: test
+// Returns true if we can assume that this is a JSON request, false otherwise.
 func JsonEncoding(req *http.Request) bool {
 	if checkHeader(req, "Content-Type") {
 		return true
