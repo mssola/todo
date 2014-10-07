a16910b5476a28c216cf72b2a5d147712298eb95
Miquel Sabaté <mikisabate@gmail.com>
2014-10-07 14:00:10 +0200

    lib: let's use the time package for the MaxAge option.

--- lib/store.go
+++ lib/store.go
@@ -6,6 +6,7 @@ package lib
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/sessions"
 	"github.com/mssola/go-utils/security"
@@ -20,13 +21,13 @@ const (
 	sessionName = "todo"
 
 	// Max-Age of a whole year.
-	maxAge = 60 * 60 * 24 * 30 * 12
+	maxAge = time.Hour * 24 * 365
 )
 
 // Initialize the global cookie store.
 func InitSession() {
 	store = sessions.NewCookieStore([]byte(security.NewAuthToken()))
-	store.Options = &sessions.Options{Path: "/", MaxAge: maxAge}
+	store.Options = &sessions.Options{Path: "/", MaxAge: int(maxAge)}
 }
 
 // Tries to get the cookie store for the given request. It panics if it fails.
