69aefead79a14333d43e7199fc9f9f05591897ac
Miquel Sabaté <msabate@suse.com>
2016-03-04 14:00:03 +0100

    Re-licensed to the MPL 2.0
    Signed-off-by: Miquel Sabaté <msabate@suse.com>

--- lib/views.go
+++ lib/views.go
@@ -1,6 +1,8 @@
 // Copyright (C) 2014-2016 Miquel Sabaté Solà <mikisabate@gmail.com>
-// This file is licensed under the MIT license.
-// See the LICENSE file.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package lib
 
