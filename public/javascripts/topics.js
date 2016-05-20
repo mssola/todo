e76b58a684f4ad8c801f5a2fc657422d30cefa35
Miquel Sabaté Solà <msabate@suse.com>
2016-05-20 10:26:52 +0200

    public/javascript: updated license header
    Signed-off-by: Miquel Sabaté Solà <msabate@suse.com>

--- public/javascripts/topics.js
+++ public/javascripts/topics.js
@@ -1,7 +1,9 @@
 /*
  * Copyright (C) 2014-2016 Miquel Sabaté Solà <mikisabate@gmail.com>
- * This file is licensed under the MIT license.
- * See the LICENSE file.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 window.onload = function() {
